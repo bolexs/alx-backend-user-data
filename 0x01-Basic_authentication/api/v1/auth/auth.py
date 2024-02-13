6cdb923db0a2414bcea3a559d66ddc53715006de
bolexs <boluwatufeadeojo@gmail.com>
2024-02-13 20:50:02 +0100

    alx basic authentications

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Auth module"""
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Require auth
+        """
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Authorization header
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Current user
+        """
+        return None
