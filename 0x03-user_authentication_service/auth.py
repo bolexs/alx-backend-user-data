fbf3fce031d64971a8052d43c6a88374acd6009d
bolexs <boluwatufeadeojo@gmail.com>
2024-02-21 19:53:24 +0100

    more  alx backend user-data

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Auth module"""
+import bcrypt
+from db import DB
+from user import User
+import uuid
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user"""
+        try:
+            self._db.find_user_by(email=email)
+        except Exception:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except Exception:
+            return False
+
+    def _generate_uuid():
+        """Generate UUID"""
+        return str(uuid.uuid4())
